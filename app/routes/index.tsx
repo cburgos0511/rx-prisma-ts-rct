41556c84e9a176fd1eedd4f5c14d8dd8fba99557
cburgos0511 <cburgos0511@gmail.com>
2022-01-30 21:07:15 -0600

    Added responsiveness on task page

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -4,7 +4,7 @@ export default function Index() {
   return (
     <section className="px-6">
       <div className="grid grid-cols-1 md:grid-cols-6 gap-4">
-        <div className="col-span-2">
+        <div className="col-span-2 md:col-span-3 sm:col-span-6">
           <h1 className="text-xl font-semibold dark:text-gray-400 text-gray-700 mb-4">
             My Flow
           </h1>
